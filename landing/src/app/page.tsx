0bb7046d7d3f44fb2dc2a71ffb091d018c50e963
Tan Le <tanlethanh8266@gmail.com>
2023-11-28 11:40:07 +0700

    feat: reconfig landing meta

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-2">
       <h1 className="text-6xl">Zenonian</h1>
+      <p>coming soon</p>
     </main>
   )
 }
